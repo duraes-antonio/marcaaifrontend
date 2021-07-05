6004d29409f339bae238fd501d2ddc6dc4a68287
duraes-antonio <garotoseis@gmail.com>
2021-07-05 01:21:49 -0300

    NEW: Página de estabelecimentos

--- src/pages/provider-details/provider-details.tsx
+++ src/pages/provider-details/provider-details.tsx
@@ -3,11 +3,33 @@ import {RouteProp} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {IdType} from '../../shared/store/types';
 import {RouteName} from '../../shared/routes/routes';
-import {IProvider} from '../../domain/entities/provider';
+import {
+    IProviderBasic,
+    providerCategoryName,
+} from '../../domain/entities/provider';
 import {Image} from 'react-native';
+import {
+    avatarSize,
+    AvatarWrapper,
+    HeaderBottomBar,
+    HeaderContainer,
+    HeaderInfo,
+    ProviderCategory,
+    ProviderName,
+    styles,
+    TagsContainer,
+} from './styles';
+import TagWorkdayStatus from '../../shared/components/tag/tag-workday';
+import TagDistance from '../../shared/components/tag/tag-distance';
+import ActionButton from '../../shared/components/buttons/action-button';
+import {IconLib, IconWrapper} from '../../shared/components/icon/icon-lib';
+import EmptyImageSvg from '../../../assets/vectors/empty_image.svg';
+import styled from 'styled-components/native';
+import ButtonContained from '../../shared/components/buttons/button';
+import {pageBackgroundColor} from '../../shared/components/general/page/styles';
 
 type ProviderParams = {
-    [RouteName.PROVIDER]: {id: IdType; item?: IProvider};
+    [RouteName.PROVIDER]: {id: IdType; item: IProviderBasic};
 };
 
 export interface ProviderPageProps {
@@ -15,16 +37,85 @@ export interface ProviderPageProps {
     navigation: StackNavigationProp<ProviderParams, RouteName.PROVIDER>;
 }
 
-const avatarSize = 120;
+function Header(props: {provider: IProviderBasic}): JSX.Element {
+    const {imageUrl, workStatus, name, category, id, distanceInMeters} =
+        props.provider;
+    return (
+        <HeaderContainer>
+            <HeaderBottomBar>
+                <AvatarWrapper style={styles.avatarImage}>
+                    {!imageUrl ? (
+                        <Image
+                            source={{uri: imageUrl ?? ''}}
+                            style={{width: avatarSize, height: avatarSize}}
+                            nativeID={`${id}-dest`}
+                        />
+                    ) : (
+                        // TODO: Se houver reuso, componentizar
+                        <EmptyImageSvg
+                            width={avatarSize - 20}
+                            height={avatarSize - 20}
+                        />
+                    )}
+                </AvatarWrapper>
+                <HeaderInfo>
+                    <ProviderName numberOfLines={2}>{name}</ProviderName>
+                    <ProviderCategory>
+                        {providerCategoryName[category]}
+                    </ProviderCategory>
+                    <TagsContainer>
+                        <TagWorkdayStatus
+                            style={{marginRight: 5}}
+                            status={workStatus}
+                        />
+                        {distanceInMeters && (
+                            <TagDistance distanceInMeters={distanceInMeters} />
+                        )}
+                    </TagsContainer>
+                </HeaderInfo>
+                <ActionButton
+                    containerStyle={styles.favoriteButton}
+                    containerShadow
+                    lib={IconLib.FEATHER}
+                    name={'heart'}
+                />
+            </HeaderBottomBar>
+        </HeaderContainer>
+    );
+}
+
+const ContainerPage = styled.View`
+    align-items: center;
+    min-height: 100%;
+    flex: 1;
+    background-color: ${pageBackgroundColor};
+`;
 
 function ProviderDetails(props: ProviderPageProps): JSX.Element {
     const {params} = props.route;
+    const showAddress = () => null;
     return (
-        <Image
-            source={{uri: params?.item?.imageUrl ?? ''}}
-            style={{width: avatarSize, height: avatarSize}}
-            nativeID={`${params.id}-dest`}
-        />
+        <ContainerPage>
+            <Header provider={params.item} />
+            <ButtonContained
+                icon={({size, color}) => (
+                    <IconWrapper
+                        size={24}
+                        color={color}
+                        name={'map-marker-alt'}
+                        lib={IconLib.FONT_AWESOME_5}
+                    />
+                )}
+                style={{
+                    height: 50,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginTop: 20,
+                }}
+                text={'Ver endereço'}
+                onPress={showAddress}
+            />
+        </ContainerPage>
     );
 }
 
---
+++ src/pages/provider-details/styles.tsx
@@ -0,0 +1,73 @@
+import {
+    colorLabel,
+    colorPrimary,
+    grey3,
+} from '../../shared/styles/global-styles';
+import {pageBackgroundColor} from '../../shared/components/general/page/styles';
+import styled from 'styled-components/native';
+import {StyleSheet} from 'react-native';
+import {CommonText, SemiBoldText} from '../../shared/components/general/texts';
+
+export const avatarSize = 110;
+
+export const AvatarWrapper = styled.View`
+    background-color: white;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const HeaderContainer = styled.View`
+    background-color: ${grey3};
+    min-height: 170px;
+    width: 100%;
+`;
+
+export const HeaderBottomBar = styled.View`
+    flex-direction: row;
+    min-height: 120px;
+    width: 100%;
+    border-top-left-radius: 90px;
+    background-color: ${pageBackgroundColor};
+    position: absolute;
+    bottom: 0;
+    padding: 0 25px;
+`;
+
+export const HeaderInfo = styled.View`
+    top: 15px;
+    flex: 1;
+    margin-left: 15px;
+`;
+
+export const ProviderName = styled(SemiBoldText)`
+    color: ${colorPrimary};
+    flex-wrap: wrap;
+`;
+
+export const ProviderCategory = styled(CommonText)`
+    color: ${colorLabel};
+    font-size: 13px;
+`;
+
+export const TagsContainer = styled.View`
+    flex-direction: row;
+    margin-top: 5px;
+`;
+
+export const styles = StyleSheet.create({
+    avatarImage: {
+        borderRadius: 15,
+        height: avatarSize,
+        width: avatarSize,
+        elevation: 9,
+        shadowColor: 'rgba(0, 0, 0, .75)',
+        overflow: 'hidden',
+        top: -18,
+    },
+    favoriteButton: {
+        position: 'absolute',
+        top: -20,
+        right: 25,
+        backgroundColor: 'white',
+    },
+});
